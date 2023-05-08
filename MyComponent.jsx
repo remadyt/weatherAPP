5a8e7ad526975dd257976804e7054886a9bb1658
remadyt <74316916+remadyt@users.noreply.github.com>
2023-05-08 15:11:09 +0300

    feat: my component

---
+++ MyComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  incrementCount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.incrementCount.bind(this)}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
